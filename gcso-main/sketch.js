bd623e9668a2259fba8fd117af54863242dd2dc6
pritam-ux <70642392+pritam-ux@users.noreply.github.com>
2020-11-27 11:08:40 +0530

    Add files via upload

--- gcso-main/sketch.js
+++ gcso-main/sketch.js
@@ -1,5 +1,5 @@
 
-var wall,thickness,damage;
+var wall,thickness;
 var speed,weight;
 
 
@@ -7,8 +7,8 @@ var speed,weight;
 function setup() {
   createCanvas(1600,400);
   
-  speed=random(223,321)
-weight=random(30,52)
+  speed=random(223,321);
+weight=random(30,52);
 thickness=random(22,83);
 bullet=createSprite(50,200,50,50);
 
@@ -18,7 +18,7 @@ bullet.shapeColor=color("white");
 
 
 wall.shapeColor=color(80,80,80)
-wall=createSprite(1200,200,thickness,height/2)
+wall=createSprite(1200, 200, thickness, height/2);
 
 }
 
